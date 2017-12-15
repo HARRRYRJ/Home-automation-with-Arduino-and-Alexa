da42cc42d7662d604cad4873d1f6ed2a0383c7a8
Harry RJ <harry2rj@gmail.com>
2017-12-16 00:06:23 +0530

    Update automation.py

--- automation.py
+++ automation.py
@@ -6,6 +6,16 @@ GPIO.setmode(GPIO.BOARD)
 
 #GPIO.setmode(GPIO.BCM)
 
+#temperature and humidity sensor(dht11)
+import sys
+import Adafruit_DHT
+
+while True:
+
+    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+
+    print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
+
 #light intensity detector
 GPIO.setup(4,GPIO.IN)
  
