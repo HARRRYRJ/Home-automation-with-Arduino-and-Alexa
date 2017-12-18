443e80fac0f61e3c5ee98856e6484156ac161558
Harry RJ <harry2rj@gmail.com>
2017-12-18 23:40:14 +0530

    Update automation.py

--- automation.py
+++ automation.py
@@ -14,6 +14,9 @@ while True:
     humidity, temperature = Adafruit_DHT.read_retry(11, 5)
 
     print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
+    
+    if (temperature>25):
+        GPIO.output(relay1, 1)#turn fan on
 
 #light intensity detector
 GPIO.setup(4,GPIO.IN)
